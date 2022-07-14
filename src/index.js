2a5051c185003ac124c54d78ec472dfdd06847f9
Opeyemi <yemilokoro@gmail.com>
2022-07-14 15:33:01 -0400

    coded current temperature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "8e88bab69fd8392de8ddb8dc9567fe60";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Baltimore&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
